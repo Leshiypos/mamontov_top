0c886d4a23acc6de777d47842affb3d1e6d83577
Wp_devsite <leshiy_pos@mail.ru>
2024-11-23 22:03:21 +0300

    Добавить Секцию Подарок

---
+++ templates/layout/present_section.php
@@ -0,0 +1,22 @@
+<?php 
+// Секция подарок
+?>
+<section class="present_section">
+	<div class="wrap"> 
+	<img src="<?php echo get_template_directory_uri(  ).'/new-site/assets/images/present_box_big.png'; ?>" alt="" class="present_box">
+		<div class="presentition">
+			<div>
+				<h2>Подарок для вас</h2>
+				<div class="descr_present">Подарки для всех, кто пройдет по ссылке. Получать качественные лиды в нише премиум авто Москва после отключение основой рекламной площадки </div>
+				<ul>
+					<li>Позиционирование</li>
+					<li>Сайт</li>
+					<li>Аналитика</li>
+					<li>SEO</li>
+					<li>Реклама</li>
+				</ul>
+				<a href="/case/internet-marketing-dlja-kottedzhnogo-posjolka/" class="btn btn__order radius_1">Получить подарок</a>
+			</div>
+		</div>
+	</div>
+</section>
