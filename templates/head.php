5a6ff82480f0b361662e48e459067c00d4b7ce4f
Wp_devsite <leshiy_pos@mail.ru>
2025-03-12 21:23:07 +0300

    перенесена библиотека swiper локально на хостинг

--- templates/head.php
+++ templates/head.php
@@ -19,13 +19,17 @@ $skip_link_url = apply_filters( 'hello_elementor_skip_link_url', '#content' );
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="<?php echo esc_attr( $viewport_content ); ?>">
+	<meta name="viewport" content="<?php echo esc_attr( $viewport_content ); ?>, maximum-scale=1.0, user-scalable=no">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
 
 	<!--SWIPER-->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
-    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/> -->
+    <link rel="stylesheet" href="<?php  echo get_template_directory_uri();?>/new-site/assets/lib/swiper/swiper-bundle.min.css"/>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script> -->
+    <script src="<?php  echo get_template_directory_uri();?>/new-site/assets/lib/swiper/swiper-bundle.min.js"></script>
+
+
 
     <!--Slick Slider-->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/new-site/assets/lib/slick/slick-theme.css">
